b5105e7d57c2992e5498fdd28774aafaaed7943e
Awinash Ragothaman <awinash@gmail.com>
2025-06-21 02:00:22 +0000

    more scaffolding

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,5 @@
-// ReactDOM.render here
+import React from "react"; 
+import ReactDOM from "react-dom/client"; 
+import { BrowserRouter } from "react-router-dom"; 
+import App from "./App"; 
+ReactDOM.createRoot(document.getElementById("root")!).render(<BrowserRouter><App /></BrowserRouter>);
